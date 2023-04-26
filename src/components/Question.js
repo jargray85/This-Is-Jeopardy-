66e7d028a6a229c23823eefcd04765f8448126e7
JamesRJGray <jamesrjgray@gmail.com>
2023-04-25 21:44:44 -0500

    created header question and score files and added to app.js

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,54 @@
-const Header = (props) => {
-    return (
-        <div className="header-container">
-            <h1>This Is Jeopardy!</h1>
-        </div>
-    )
+import { useEffect, useState } from "react"
+
+const Question = (props) => {
+
+    // Random question API url
+    const url = "http://jservice.io/api/random"
+
+    // Set state
+    const [question, setQuestion] = useState()
+        // set SECOND state with answer
+    const [showAnswer, setAnswer] = useState(false)
+
+    const getQuestion = async () => {
+        // fetch API
+        const response = await fetch(url)
+        // get question
+        const data = await response.json()
+        // set state to question
+        setQuestion(data)
+            // and answer
+        setAnswer(false)
+    }
+
+    useEffect(() => {
+        getQuestion()
+    }, [])
+
+    // define toggle function
+    const toggleAnswer = () => setAnswer(!showAnswer)
+
+    const Loaded = () => {    
+        return (
+            <div className="question-container">
+                <h2>Let's Play!</h2>
+                <section>
+                    <button onClick={getQuestion}>Get Answer</button>
+                </section>
+                <h3> Category: {question[0].category.title} </h3>
+                <h3> Points: {question[0].value} </h3>
+                <h3> Answer: {question[0].question} </h3>
+                <section>
+                    <button onClick={toggleAnswer}>Get Question</button>
+                </section>
+                {showAnswer && <h3> {question[0].answer} </h3>}
+            </div>
+        )
+    }
+
+    const Loading = () => <h1>Loading...</h1>
+
+    return question ? <Loaded /> : <Loading />
 }
 
-export default Header
+export default Question
---
+++ src/components/Score.js
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+const Score = (props) => {
+
+    //Array destructuring
+    const [score, setScore] = useState(0)
+
+    // Add score 
+    const addScore = () => {setScore(score + 100)}
+
+    // Subtract score
+    const subScore = () => {setScore(score - 100)}
+
+    const resetScore = () => {setScore(0)}
+
+    return (
+        <div className="score-container">
+            <h2>Score: { score } </h2>
+            <section>
+                <button onClick={addScore}>Increase</button>
+                <button onClick={subScore}>Decrease</button>
+                <button onClick={resetScore}>Reset</button>
+            </section>
+        </div>
+    )
+}
+
+export default Score
